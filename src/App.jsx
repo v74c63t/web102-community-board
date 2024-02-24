7f9cad40c0016522ea4566c9da9b3ab01f5014fb
Vanessa Tang <vanesst5@uci.edu>
2024-02-24 12:41:30 -0800

    Added basic title/styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import './App.css'
+import Card from './components/Card'
 
 function App() {
 
+  const title = 'title'
+  const description = 'description'
+  const link = '#'
 
   return (
     <>
-      
+     <h1 className="title">COMMUNITY BOARD</h1>
+     <Card title={title} description={description} link={link} /> 
     </>
   )
 }
